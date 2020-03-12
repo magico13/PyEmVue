eea46f4fb0d7027c4b6c656b1995296a9e7a880a
magico13 <magico1313@gmail.com>
2020-03-12 18:49:16 -0400

    Add main file for direct invoking

---
+++ pyemvue/__main__.py
@@ -0,0 +1,75 @@
+import sys
+import datetime
+import json
+
+# Our files
+from enums import Scale, Unit, TotalTimeFrame, TotalUnit
+from customer import Customer
+from device import VueDevice, VueDeviceChannel, VuewDeviceChannelUsage
+from pyemvue import PyEmVue
+
+def main():
+    errorMsg = 'Please pass a file containing the "email" and "password" as json.'
+    if len(sys.argv) == 1:
+        print(errorMsg)
+        sys.exit(1)
+
+    filepath = sys.argv[1]
+
+    data = {}
+    email = None
+    passw = None
+    idToken = None
+    accessToken = None
+    refreshToken = None
+    try:
+        with open(filepath) as f:
+            data = json.load(f)
+    except:
+        print('Error opening file.', errorMsg)
+        raise
+    if ('email' not in data or 'password' not in data) and ('idToken' not in data or 'accessToken' not in data or 'refreshToken' not in data):
+        print(errorMsg)
+        sys.exit(1)
+    canLogIn = False
+    if 'email' in data:
+        email = data['email']
+        if 'password' in data:
+            passw = data['password']
+            canLogIn = True
+    if 'idToken' in data and 'accessToken' in data and 'refreshToken' in data:
+        idToken = data['idToken']
+        accessToken = data['accessToken']
+        refreshToken = data['refreshToken']
+        canLogIn = True
+    if not canLogIn:
+        print('Not enough details to log in.', errorMsg)
+        sys.exit(1)
+    vue = PyEmVue()
+    vue.login(email, passw, idToken, accessToken, refreshToken, token_storage_file='keys.json')
+    print('Logged in. Authtoken follows:')
+    print(vue.cognito.id_token)
+    print()
+    devices = vue.get_devices()
+    for device in devices:
+        print(device.device_gid, device.manufacturer_id, device.model, device.firmware)
+        for chan in device.channels:
+            print('\t', chan.device_gid, chan.name, chan.channel_num, chan.channel_multiplier)
+    print(vue.get_total_usage(devices[0].channels[0], TotalTimeFrame.MONTH.value) / 1000, 'kwh used month to date')
+    print(vue.get_total_usage(devices[0].channels[0], TotalTimeFrame.ALL.value) / 1000, 'kwh used total')
+    now = datetime.datetime.utcnow()
+    minAgo = now - datetime.timedelta(minutes=1)
+    print('Total usage over the last day in kwh: ')
+    use = vue.get_recent_usage(Scale.DAY.value)
+    for chan in use:
+        print(f'{chan.device_gid} ({chan.channel_num}): {chan.usage/1000} kwh')
+    print('Average usage over the last minute in watts: ')
+    use = vue.get_recent_usage(Scale.MINUTE.value)
+    for chan in use:
+        print(f'{chan.device_gid} ({chan.channel_num}): {chan.usage} W')
+    
+    print('Usage over the last minute in watts: ', vue.get_usage_over_time(devices[0].channels[0], minAgo, now))
+    
+
+if __name__ == '__main__':
+    main()
--- pyemvue/pyemvue.py
+++ pyemvue/pyemvue.py
@@ -1,8 +1,6 @@
 import requests
 import datetime
-from enum import Enum
 import json
-import sys
 
 # These provide AWS cognito authentication support
 import boto3
@@ -150,58 +148,3 @@ class PyEmVue(object):
 
 def _format_time(time):
     return time.isoformat()+'Z'
-
-if __name__ == '__main__':
-    data = {}
-    email = None
-    passw = None
-    idToken = None
-    accessToken = None
-    refreshToken = None
-    try:
-        with open('keys.json') as f:
-            data = json.load(f)
-    except:
-        print('Please create a "keys.json" file containing the "email" and "password"')
-        sys.exit(1)
-    if ('email' not in data or 'password' not in data) and ('idToken' not in data or 'accessToken' not in data or 'refreshToken' not in data):
-        print('Please create a "keys.json" file containing the "email" and "password"')
-        sys.exit(1)
-    canLogIn = False
-    if 'email' in data:
-        email = data['email']
-        if 'password' in data:
-            passw = data['password']
-            canLogIn = True
-    if 'idToken' in data and 'accessToken' in data and 'refreshToken' in data:
-        idToken = data['idToken']
-        accessToken = data['accessToken']
-        refreshToken = data['refreshToken']
-        canLogIn = True
-    if not canLogIn:
-        print('Please create a "keys.json" file containing the "email" and "password"')
-        sys.exit(1)
-    vue = PyEmVue()
-    vue.login(email, passw, idToken, accessToken, refreshToken, token_storage_file='keys.json')
-    print('Logged in. Authtoken follows:')
-    print(vue.cognito.id_token)
-    print()
-    devices = vue.get_devices()
-    for device in devices:
-        print(device.device_gid, device.manufacturer_id, device.model, device.firmware)
-        for chan in device.channels:
-            print('\t', chan.device_gid, chan.name, chan.channel_num, chan.channel_multiplier)
-    print(vue.get_total_usage(devices[0].channels[0], TotalTimeFrame.MONTH.value) / 1000, 'kwh used month to date')
-    print(vue.get_total_usage(devices[0].channels[0], TotalTimeFrame.ALL.value) / 1000, 'kwh used total')
-    now = datetime.datetime.utcnow()
-    minAgo = now - datetime.timedelta(minutes=1)
-    print('Total usage over the last day in kwh: ')
-    use = vue.get_recent_usage(Scale.DAY.value)
-    for chan in use:
-        print(f'{chan.device_gid} ({chan.channel_num}): {chan.usage/1000} kwh')
-    print('Average usage over the last minute in watts: ')
-    use = vue.get_recent_usage(Scale.MINUTE.value)
-    for chan in use:
-        print(f'{chan.device_gid} ({chan.channel_num}): {chan.usage} W')
-    
-    print('Usage over the last minute in watts: ', vue.get_usage_over_time(devices[0].channels[0], minAgo, now))
