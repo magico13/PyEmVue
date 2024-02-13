1d0e44ff5093be9db824947acf6d1c15f13432dd
DabblerIOT <159593555+DabblerIOT@users.noreply.github.com>
2024-02-12 20:29:58 -0500

    Update pyemvue.py

--- pyemvue/pyemvue.py
+++ pyemvue/pyemvue.py
@@ -101,7 +101,7 @@ class PyEmVue(object):
         devices: dict[int, VueUsageDevice] = {}
         if response.text:
             j = response.json()
-            if (len(j) == len(gids))
+            if (len(j) == len(gids)):
                 if 'deviceListUsages' in j and 'devices' in j['deviceListUsages']:
                     timestamp = parse(j['deviceListUsages']['instant'])
                     for device in j['deviceListUsages']['devices']:
